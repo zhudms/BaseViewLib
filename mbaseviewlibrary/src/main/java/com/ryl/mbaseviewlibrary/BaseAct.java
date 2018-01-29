6f42431700004165ee2fb6b058fc33ec5efd2987
TangXingmeng <zhudms@163.com>
2018-01-29 14:03:04 +0800

    add

---
+++ mbaseviewlibrary/src/main/java/com/ryl/mbaseviewlibrary/BaseAct.java
@@ -0,0 +1,28 @@
+package com.ryl.mbaseviewlibrary;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by rongyile on 2017/12/21.
+ */
+
+public abstract class BaseAct extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(getLayoutID());
+
+
+    }
+
+    protected abstract int getLayoutID();
+
+
+}
---
+++ mbaseviewlibrary/src/main/java/com/ryl/mbaseviewlibrary/BaseRecycleAdapter.java
@@ -0,0 +1,23 @@
+package com.ryl.mbaseviewlibrary;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * Created by rongyile on 2018/1/29.
+ */
+
+public abstract  class BaseRecycleAdapter<T> extends RecyclerView.Adapter<BaseRecycleHolder> {
+
+    protected List<T> mLists;
+    protected Context mContext;
+
+    public BaseRecycleAdapter(List<T> mLists, Context mContext) {
+        this.mLists = mLists;
+        this.mContext = mContext;
+    }
+
+
+}
